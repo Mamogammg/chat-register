08c8b0ee92b708ba8197cdbcae9c880aba2cd998
Mamogammg <mario.montaner.ganan@gmail.com>
2025-04-25 16:12:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ let data = {};
 const server = http.createServer((req, res) => {
   console.log(req.url)
   console.log(req.headers['user-agent'])
-  if (req.url === '/' || req.url === '' || req.url.includes('/?')) {
+  if (req.url === '/' || req.url === '' || req.url.includes('app/?')) {
     const indexPath = path.join(__dirname, 'index.html');
 
     // Leer el archivo "index.html" de manera asíncrona
@@ -35,6 +35,42 @@ const server = http.createServer((req, res) => {
       res.setHeader("response",JSON.stringify(data))
       res.writeHead(200, { 'Content-Type': 'text/plain' },);
       res.end(JSON.stringify(data))
+    } else if (req.method === 'POST') {
+      let body = "";
+      req.on('data', (chunk) => {
+        body += chunk;
+      });
+
+      req.on('end', () => {
+        try {
+          body = JSON.parse(body)
+          console.log(body)
+          var [clave, valor] = Object.entries(body)[0]
+          var id = body["id"]
+          data[clave] = {[id]: valor}
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ success: true, message: 'Data saved successfully' }));
+        } catch (e) {
+          console.log(e)
+          res.writeHead(400, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify({ success: false, message: 'Invalid JSON format' }));
+        }
+      })
+    }
+  } else if (req.url.includes('/get_code')) {
+    if (req.method === 'GET') {
+      const indexPath = path.join(__dirname, 'get_code.html');
+
+    // Leer el archivo "index.html" de manera asíncrona
+    fs.readFile(indexPath, 'utf8', (err, content) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error interno del servidor');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    });
     } else if (req.method === 'POST') {
       let body = "";
       req.on('data', (chunk) => {
